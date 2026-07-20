9d869a8ff1bca98d351a229a98ab10c76e315d57
Brijnandan11 <brij19069@gmail.com>
2026-07-21 02:29:35 +0530

    setup app.ts

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from "express"
+import helmet from "helmet"
+import cors from "cors"
+
+import routes from "./routes"
+import { notFoundMiddleware } from "./middlewares/notFound.middleware"
+import { errorMiddleware } from "./middlewares/error.middleware"
+
+const app = express()
+
+app.use(helmet())
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use('/api/v1', routes)
+
+app.use(notFoundMiddleware)
+
+app.use(errorMiddleware)
+
+export default app
