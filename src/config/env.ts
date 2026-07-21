f1362ea66c92acc7d25f8c871287f113ad53c93a
Brijnandan11 <brij19069@gmail.com>
2026-07-21 09:43:25 +0530

    setup server

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,3 +1,15 @@
 import dotenv from "dotenv"
+import { z } from "zod"
 
 dotenv.config()
+
+const envSchema = z.object({
+    NODE_ENV: z.enum(['development', 'production', 'test']),
+    PORT: z.coerce.number(),
+    DATABASE_URL: z.string(),
+    REDIS_URL: z.string(),
+    LOG_LEVEL: z.string(),
+    JWT_SECRET: z.string(),
+})
+
+export const env = envSchema.parse(process.env)
---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import { env } from "./config/env"
+import app from "./app"
+
+const server = app.listen(env.PORT, () =>{
+    console.log(`Server is running on port ${env.PORT}`)
+})
+
+export default server
