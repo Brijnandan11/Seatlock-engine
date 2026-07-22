eb2cd1ced02d5bfa937cb8305d67605f75cc8e6e
Brijnandan11 <brij19069@gmail.com>
2026-07-22 15:23:58 +0530

    add req middleware

---
+++ src/middlewares/requestId.middleware.ts
@@ -0,0 +1,12 @@
+import { randomUUID } from "crypto"
+import { NextFunction, Request, Response} from "express"
+
+export function requestIdMiddleware(req: Request, res: Response, next: NextFunction){
+
+    const requestId = randomUUID()
+    
+    req.headers['x-request-id'] = requestId
+    res.setHeader('X-Request-ID', requestId)
+
+    next()
+}
