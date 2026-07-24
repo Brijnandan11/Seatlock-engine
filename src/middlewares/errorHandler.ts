7995b8d2f74e29b21fd0ae3d3fb25c26749610e1
Brijnandan11 <brij19069@gmail.com>
2026-07-24 23:24:54 +0530

    add errorhandler

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,32 @@
+import { Response, Request, NextFunction } from "express"
+import { AppError } from "../errors"
+import { logger } from "../lib/logger"
+import { env } from "../config/env"
+
+export function errorHandler(
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+){
+    if(err instanceof AppError && err.isOperational){
+        logger.warn({err, reqId: req.id}, "Operational error")
+
+        return res.status(err.httpStatus).json({
+            error: {
+                code: err.code,
+                message: err.message,
+                details: err.details
+            }
+        })
+    }
+    logger.error({ err, reqId: req.id }, 'Unhandled error')
+
+    return res.status(500).json({
+        error:{
+            code: "INTERNAL_ERROR",
+            message: env.NODE_ENV === "production" ? "Somehting went wrong" : err.message
+        }
+    })
+        
+}
