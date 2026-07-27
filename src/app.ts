1e5abfc471ef553f3ad6c10181d2c8df1ac7d0ec
Brijnandan11 <brij19069@gmail.com>
2026-07-27 16:12:40 +0530

    mount authRoute

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { pinoHttp } from 'pino-http'
 import { logger } from './lib/logger'
 import { healthRouter } from './routes/health.routes'
 import { errorHandler } from './middlewares/errorHandler'
+import { authRouter } from './routes/auth.routes'
 
 export const app = express()
 
@@ -12,3 +13,5 @@ app.use(pinoHttp({logger}))
 app.use(healthRouter)
 
 app.use(errorHandler)
+
+app.use('/auth', authRouter)
