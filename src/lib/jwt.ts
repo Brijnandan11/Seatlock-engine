ef1c3e31f07d95824399cb2ce597c06225b1fa0b
Brijnandan11 <brij19069@gmail.com>
2026-07-28 11:21:53 +0530

    add token verification fn

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -1,14 +1,26 @@
 import jwt, { type SignOptions } from 'jsonwebtoken'
 import { env } from '../config/env'
+import { InvalidTokenError, TokenExpiredError } from '../errors'
 
-export interface JwtPayLoad{
+export interface JwtPayload{
     userId: string,
     role: string
 }
 
-export function signToken(payload: JwtPayLoad): string {
+export function signToken(payload: JwtPayload): string {
      return jwt.sign(payload, env.JWT_SECRET, {
         expiresIn: env.JWT_EXPIRES_IN as SignOptions['expiresIn'],
         algorithm: 'HS256',
      })
+}
+
+export function verifyToken(token: string): JwtPayload {
+  try {
+    return jwt.verify(token, env.JWT_SECRET, { algorithms: ['HS256'] }) as JwtPayload;
+  } catch (err) {
+    if (err instanceof jwt.TokenExpiredError) {
+      throw new TokenExpiredError();
+    }
+    throw new InvalidTokenError();
+  }
 }
