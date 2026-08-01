c4c63b5c91e1ebb7e8befbfb69dfe81a7a8313c7
Brijnandan11 <brij19069@gmail.com>
2026-08-01 22:41:44 +0530

    error fix

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,7 +1,6 @@
 import Redis from "ioredis"
 import { env } from '../config/env'
 import { logger } from "./logger"
-import { error } from "node:console"
 
 export const redis = new Redis(env.REDIS_URL, {
     maxRetriesPerRequest:3,
