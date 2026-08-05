4fe64c1ad49bb2deb58c8837f3ae9071c673b266
Brijnandan11 <brij19069@gmail.com>
2026-08-05 22:45:14 +0530

    add venue repository

---
+++ src/repositories/venue.repository.ts
@@ -0,0 +1,17 @@
+import { eq } from 'drizzle-orm'
+import { db } from '../lib/db'
+import { venues, NewVenus } from '../db/schema/venues'
+
+export async function insertVenue(data: NewVenus) {
+    const result = await db.insert(venues).values(data).returning()
+    return result[0]
+}
+
+export async function findVenueById(id: string) {
+    const result = await db.select().from(venues).where(eq(venues.id, id)).limit(1)
+    return result[0] ?? null
+}
+
+export async function findAllVenues() {
+    return db.select().from(venues)
+}
