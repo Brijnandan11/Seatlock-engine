77a55b486951a28628dec151e8246fbce264cb56
Brijnandan11 <brij19069@gmail.com>
2026-08-08 14:57:03 +0530

    mount API routers under v1

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,10 @@ import { healthRouter } from './routes/health.routes'
 import { errorHandler } from './middlewares/errorHandler'
 import { authRouter } from './routes/auth.routes'
 import { sectionRouter } from './routes/section.routes'
+import { eventRouter } from './routes/event.routes'
+import { venueRouter } from './routes/venue.routes'
+import { showRouter } from './routes/show.routes'
+
 
 export const app = express()
 
@@ -15,5 +19,9 @@ app.use(healthRouter)
 app.use('/auth', authRouter)
 
 app.use('/v1', sectionRouter)
+app.use('/v1', venueRouter);
+app.use('/v1', eventRouter);
+app.use('/v1', showRouter);
+
 
 app.use(errorHandler)
