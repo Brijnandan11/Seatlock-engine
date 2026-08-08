5a15d863ba05080173dbbbff5f37a666ca7931e8
Brijnandan11 <brij19069@gmail.com>
2026-08-08 14:18:12 +0530

    add admin-only show creation route

---
+++ src/routes/show.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express"
+import { catchAsync } from "../middlewares/catchAsync"
+import { authenticate } from "../middlewares/auth.middleware"
+import { requireRole } from "../middlewares/rbac.middleware"
+import { createShowSchema } from "../schemas/show.schema"
+import { createShow, getShowById } from "../services/show.service"
+
+export const showRouter = Router()
+
+showRouter.post('/shows', authenticate, requireRole('admin'), catchAsync(async(req, res)=>{
+    const input = createShowSchema.parse(req.body)
+
+    const show = await createShow(input)
+    
+    res.status(201).json({ show })
+}))
